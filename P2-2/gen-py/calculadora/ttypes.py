028e566518499e629155aaa793d49ef189096687
ramongarver <ramongarver2000@gmail.com>
2021-04-16 16:56:08 +0200

    Terminada versión python

--- P2-2/gen-py/calculadora/ttypes.py
+++ P2-2/gen-py/calculadora/ttypes.py
@@ -14,5 +14,371 @@ import sys
 
 from thrift.transport import TTransport
 all_structs = []
+
+
+class Operation(object):
+    SUMAR = 1
+    RESTAR = 2
+    MULTIPLICAR = 3
+    DIVIDIR = 4
+    POTENCIA = 5
+    RAIZ = 6
+
+    _VALUES_TO_NAMES = {
+        1: "SUMAR",
+        2: "RESTAR",
+        3: "MULTIPLICAR",
+        4: "DIVIDIR",
+        5: "POTENCIA",
+        6: "RAIZ",
+    }
+
+    _NAMES_TO_VALUES = {
+        "SUMAR": 1,
+        "RESTAR": 2,
+        "MULTIPLICAR": 3,
+        "DIVIDIR": 4,
+        "POTENCIA": 5,
+        "RAIZ": 6,
+    }
+
+
+class Power(object):
+    """
+    Attributes:
+     - base
+     - exponente
+
+    """
+
+
+    def __init__(self, base=None, exponente=None,):
+        self.base = base
+        self.exponente = exponente
+
+    def read(self, iprot):
+        if iprot._fast_decode is not None and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None:
+            iprot._fast_decode(self, iprot, [self.__class__, self.thrift_spec])
+            return
+        iprot.readStructBegin()
+        while True:
+            (fname, ftype, fid) = iprot.readFieldBegin()
+            if ftype == TType.STOP:
+                break
+            if fid == 1:
+                if ftype == TType.I32:
+                    self.base = iprot.readI32()
+                else:
+                    iprot.skip(ftype)
+            elif fid == 2:
+                if ftype == TType.I32:
+                    self.exponente = iprot.readI32()
+                else:
+                    iprot.skip(ftype)
+            else:
+                iprot.skip(ftype)
+            iprot.readFieldEnd()
+        iprot.readStructEnd()
+
+    def write(self, oprot):
+        if oprot._fast_encode is not None and self.thrift_spec is not None:
+            oprot.trans.write(oprot._fast_encode(self, [self.__class__, self.thrift_spec]))
+            return
+        oprot.writeStructBegin('Power')
+        if self.base is not None:
+            oprot.writeFieldBegin('base', TType.I32, 1)
+            oprot.writeI32(self.base)
+            oprot.writeFieldEnd()
+        if self.exponente is not None:
+            oprot.writeFieldBegin('exponente', TType.I32, 2)
+            oprot.writeI32(self.exponente)
+            oprot.writeFieldEnd()
+        oprot.writeFieldStop()
+        oprot.writeStructEnd()
+
+    def validate(self):
+        if self.base is None:
+            raise TProtocolException(message='Required field base is unset!')
+        if self.exponente is None:
+            raise TProtocolException(message='Required field exponente is unset!')
+        return
+
+    def __repr__(self):
+        L = ['%s=%r' % (key, value)
+             for key, value in self.__dict__.items()]
+        return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        return not (self == other)
+
+
+class Calculo(object):
+    """
+    Attributes:
+     - num1
+     - num2
+     - operation
+     - result
+
+    """
+
+
+    def __init__(self, num1=None, num2=None, operation=None, result=float(0),):
+        self.num1 = num1
+        self.num2 = num2
+        self.operation = operation
+        self.result = result
+
+    def read(self, iprot):
+        if iprot._fast_decode is not None and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None:
+            iprot._fast_decode(self, iprot, [self.__class__, self.thrift_spec])
+            return
+        iprot.readStructBegin()
+        while True:
+            (fname, ftype, fid) = iprot.readFieldBegin()
+            if ftype == TType.STOP:
+                break
+            if fid == 1:
+                if ftype == TType.I32:
+                    self.num1 = iprot.readI32()
+                else:
+                    iprot.skip(ftype)
+            elif fid == 2:
+                if ftype == TType.I32:
+                    self.num2 = iprot.readI32()
+                else:
+                    iprot.skip(ftype)
+            elif fid == 3:
+                if ftype == TType.I32:
+                    self.operation = iprot.readI32()
+                else:
+                    iprot.skip(ftype)
+            elif fid == 4:
+                if ftype == TType.DOUBLE:
+                    self.result = iprot.readDouble()
+                else:
+                    iprot.skip(ftype)
+            else:
+                iprot.skip(ftype)
+            iprot.readFieldEnd()
+        iprot.readStructEnd()
+
+    def write(self, oprot):
+        if oprot._fast_encode is not None and self.thrift_spec is not None:
+            oprot.trans.write(oprot._fast_encode(self, [self.__class__, self.thrift_spec]))
+            return
+        oprot.writeStructBegin('Calculo')
+        if self.num1 is not None:
+            oprot.writeFieldBegin('num1', TType.I32, 1)
+            oprot.writeI32(self.num1)
+            oprot.writeFieldEnd()
+        if self.num2 is not None:
+            oprot.writeFieldBegin('num2', TType.I32, 2)
+            oprot.writeI32(self.num2)
+            oprot.writeFieldEnd()
+        if self.operation is not None:
+            oprot.writeFieldBegin('operation', TType.I32, 3)
+            oprot.writeI32(self.operation)
+            oprot.writeFieldEnd()
+        if self.result is not None:
+            oprot.writeFieldBegin('result', TType.DOUBLE, 4)
+            oprot.writeDouble(self.result)
+            oprot.writeFieldEnd()
+        oprot.writeFieldStop()
+        oprot.writeStructEnd()
+
+    def validate(self):
+        if self.num1 is None:
+            raise TProtocolException(message='Required field num1 is unset!')
+        if self.operation is None:
+            raise TProtocolException(message='Required field operation is unset!')
+        if self.result is None:
+            raise TProtocolException(message='Required field result is unset!')
+        return
+
+    def __repr__(self):
+        L = ['%s=%r' % (key, value)
+             for key, value in self.__dict__.items()]
+        return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        return not (self == other)
+
+
+class Point(object):
+    """
+    Attributes:
+     - x
+     - y
+     - z
+
+    """
+
+
+    def __init__(self, x=None, y=None, z=None,):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def read(self, iprot):
+        if iprot._fast_decode is not None and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None:
+            iprot._fast_decode(self, iprot, [self.__class__, self.thrift_spec])
+            return
+        iprot.readStructBegin()
+        while True:
+            (fname, ftype, fid) = iprot.readFieldBegin()
+            if ftype == TType.STOP:
+                break
+            if fid == 1:
+                if ftype == TType.DOUBLE:
+                    self.x = iprot.readDouble()
+                else:
+                    iprot.skip(ftype)
+            elif fid == 2:
+                if ftype == TType.DOUBLE:
+                    self.y = iprot.readDouble()
+                else:
+                    iprot.skip(ftype)
+            elif fid == 3:
+                if ftype == TType.DOUBLE:
+                    self.z = iprot.readDouble()
+                else:
+                    iprot.skip(ftype)
+            else:
+                iprot.skip(ftype)
+            iprot.readFieldEnd()
+        iprot.readStructEnd()
+
+    def write(self, oprot):
+        if oprot._fast_encode is not None and self.thrift_spec is not None:
+            oprot.trans.write(oprot._fast_encode(self, [self.__class__, self.thrift_spec]))
+            return
+        oprot.writeStructBegin('Point')
+        if self.x is not None:
+            oprot.writeFieldBegin('x', TType.DOUBLE, 1)
+            oprot.writeDouble(self.x)
+            oprot.writeFieldEnd()
+        if self.y is not None:
+            oprot.writeFieldBegin('y', TType.DOUBLE, 2)
+            oprot.writeDouble(self.y)
+            oprot.writeFieldEnd()
+        if self.z is not None:
+            oprot.writeFieldBegin('z', TType.DOUBLE, 3)
+            oprot.writeDouble(self.z)
+            oprot.writeFieldEnd()
+        oprot.writeFieldStop()
+        oprot.writeStructEnd()
+
+    def validate(self):
+        if self.x is None:
+            raise TProtocolException(message='Required field x is unset!')
+        if self.y is None:
+            raise TProtocolException(message='Required field y is unset!')
+        if self.z is None:
+            raise TProtocolException(message='Required field z is unset!')
+        return
+
+    def __repr__(self):
+        L = ['%s=%r' % (key, value)
+             for key, value in self.__dict__.items()]
+        return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        return not (self == other)
+
+
+class InvalidOperation(TException):
+    """
+    Attributes:
+     - why
+
+    """
+
+
+    def __init__(self, why=None,):
+        self.why = why
+
+    def read(self, iprot):
+        if iprot._fast_decode is not None and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None:
+            iprot._fast_decode(self, iprot, [self.__class__, self.thrift_spec])
+            return
+        iprot.readStructBegin()
+        while True:
+            (fname, ftype, fid) = iprot.readFieldBegin()
+            if ftype == TType.STOP:
+                break
+            if fid == 1:
+                if ftype == TType.STRING:
+                    self.why = iprot.readString().decode('utf-8') if sys.version_info[0] == 2 else iprot.readString()
+                else:
+                    iprot.skip(ftype)
+            else:
+                iprot.skip(ftype)
+            iprot.readFieldEnd()
+        iprot.readStructEnd()
+
+    def write(self, oprot):
+        if oprot._fast_encode is not None and self.thrift_spec is not None:
+            oprot.trans.write(oprot._fast_encode(self, [self.__class__, self.thrift_spec]))
+            return
+        oprot.writeStructBegin('InvalidOperation')
+        if self.why is not None:
+            oprot.writeFieldBegin('why', TType.STRING, 1)
+            oprot.writeString(self.why.encode('utf-8') if sys.version_info[0] == 2 else self.why)
+            oprot.writeFieldEnd()
+        oprot.writeFieldStop()
+        oprot.writeStructEnd()
+
+    def validate(self):
+        return
+
+    def __str__(self):
+        return repr(self)
+
+    def __repr__(self):
+        L = ['%s=%r' % (key, value)
+             for key, value in self.__dict__.items()]
+        return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        return not (self == other)
+all_structs.append(Power)
+Power.thrift_spec = (
+    None,  # 0
+    (1, TType.I32, 'base', None, None, ),  # 1
+    (2, TType.I32, 'exponente', None, None, ),  # 2
+)
+all_structs.append(Calculo)
+Calculo.thrift_spec = (
+    None,  # 0
+    (1, TType.I32, 'num1', None, None, ),  # 1
+    (2, TType.I32, 'num2', None, None, ),  # 2
+    (3, TType.I32, 'operation', None, None, ),  # 3
+    (4, TType.DOUBLE, 'result', None, float(0), ),  # 4
+)
+all_structs.append(Point)
+Point.thrift_spec = (
+    None,  # 0
+    (1, TType.DOUBLE, 'x', None, None, ),  # 1
+    (2, TType.DOUBLE, 'y', None, None, ),  # 2
+    (3, TType.DOUBLE, 'z', None, None, ),  # 3
+)
+all_structs.append(InvalidOperation)
+InvalidOperation.thrift_spec = (
+    None,  # 0
+    (1, TType.STRING, 'why', 'UTF8', None, ),  # 1
+)
 fix_spec(all_structs)
 del all_structs
--- P2-2/gen-py/servidor.py
+++ P2-2/gen-py/servidor.py
@@ -1,7 +1,10 @@
 import glob
 import sys
 
+import math
+
 from calculadora import Calculadora
+from calculadora.ttypes import Power, Operation, Calculo, Point, InvalidOperation
 
 from thrift.transport import TSocket
 from thrift.transport import TTransport
@@ -21,13 +24,138 @@ class CalculadoraHandler:
         print("me han hecho ping()")
 
     def suma(self, n1, n2):
-        print("sumando " + str(n1) + " con " + str(n2))
+        print("sumando", n1, "con", n2)
         return n1 + n2
 
     def resta(self, n1, n2):
-        print("restando " + str(n1) + " con " + str(n2))
+        print("restando", n1, "con", n2)
         return n1 - n2
 
+    def multiplicacion(self, n1, n2):
+        print("multiplicando", n1, "con", n2)
+        return n1 * n2
+
+    def division(self, n1, n2):
+        if n2 == 0:
+            raise InvalidOperation("InvalidOperation: No se puede dividir entre 0")
+        print("dividiendo", n1, "entre", n2)
+        return int(n1 / n2)
+
+    def potencia(self, p):
+        if p.base == 0 and p.exponente == 0:
+            raise InvalidOperation("InvalidOperation: No se puede resolver la operación 0 elevado a 0 (indeterminación)")
+        return pow(p.base, p.exponente)
+
+    def raizCuadrada(self, num):
+        if num < 0:
+            raise InvalidOperation("InvalidOperation: No se puede resolver una raíz negativa")
+        return math.sqrt(num)
+
+    def resolverCalculos(self, calculos):
+        for calculo in calculos:
+            if calculo.operation == Operation.SUMAR:
+                calculo.result = self.suma(calculo.num1, calculo.num2)
+            elif calculo.operation == Operation.RESTAR:
+                calculo.result = self.resta(calculo.num1, calculo.num2)
+            elif calculo.operation == Operation.MULTIPLICAR:
+                calculo.result = self.multiplicacion(calculo.num1, calculo.num2)
+            elif calculo.operation == Operation.DIVIDIR:
+                calculo.result = self.division(calculo.num1, calculo.num2)
+            elif calculo.operation == Operation.POTENCIA:
+                calculo.result = self.potencia(Power(calculo.num1, calculo.num2))
+            elif calculo.operation == Operation.RAIZ:
+                calculo.result = self.raizCuadrada(calculo.num1)
+        return calculos
+
+    def transladar(self, p, x, y, z):
+        result = Point(0.0, 0.0, 0.0)
+        result.x = p.x + x
+        result.y = p.y + y
+        result.z = p.z + z
+        return result
+
+    def escalar(self, p, x, y, z):
+        result = Point(0.0, 0.0, 0.0)
+        result.x = p.x * x
+        result.y = p.y * y
+        result.z = p.z * z
+        return result
+
+    def sumarVectores(self, v1, v2):
+        if len(v1) != len(v2):
+            raise InvalidOperation("InvalidOperation: No se pueden sumar vectores con distinto tamaño")
+        print(v1, "+", v2)
+        return [round(x,4) for x in [self.suma(a, b) for a, b in zip(v1, v2)]]
+
+    def restarVectores(self, v1, v2):
+        if len(v1) != len(v2):
+            raise InvalidOperation("InvalidOperation: No se pueden restar vectores con distinto tamaño")
+        print(v1, "-", v2)
+        return [round(x,4) for x in [self.resta(a, b) for a, b in zip(v1, v2)]]
+
+    def multiplicarVectores(self, v1, v2):
+        if len(v1) != len(v2):
+            raise InvalidOperation("InvalidOperation: No se pueden multiplicar vectores con distinto tamaño")
+        print(v1, "x", v2)
+        return [round(x,4) for x in [self.multiplicacion(a, b) for a, b in zip(v1, v2)]]
+
+    def dividirVectores(self, v1, v2):
+        if len(v1) != len(v2):
+            raise InvalidOperation("InvalidOperation: No se pueden dividir vectores con distinto tamaño")
+        print(v1, "/", v2)
+        return [round(x,4) for x in [self.division(a, b) for a, b in zip(v1, v2)]]
+
+    def sumarMatrices(self, m1, m2):
+        if len(m1) != len(m2):
+            raise InvalidOperation("InvalidOperation: No se pueden sumar matrices de distintas dimensiones")
+        else:
+            for i in range(len(m1)):
+                if len(m1[i]) != len(m2[i]):
+                    raise InvalidOperation("InvalidOperation: No se pueden sumar matrices de distintas dimensiones")
+
+        result = [[] for i in range(len(m1))]
+        for i in range(len(m1)):
+            result[i] = [round(x,4) for x in [self.suma(a, b) for a, b in zip(m1[i], m2[i])]]
+        return result
+
+    def restarMatrices(self, m1, m2):
+        if len(m1) != len(m2):
+            raise InvalidOperation("InvalidOperation: No se pueden sumar matrices de distintas dimensiones")
+        else:
+            for i in range(len(m1)):
+                if len(m1[i]) != len(m2[i]):
+                    raise InvalidOperation("InvalidOperation: No se pueden restar matrices de distintas dimensiones")
+
+        result = [[] for i in range(len(m1))]
+        for i in range(len(m1)):
+            result[i] = [round(x,4) for x in [self.resta(a, b) for a, b in zip(m1[i], m2[i])]]
+        return result
+
+    def multiplicarMatrices(self, m1, m2):
+        for i in range(len(m1)):
+            if len(m1[i]) != len(m2):
+                raise InvalidOperation("InvalidOperation: No se pueden multiplicar matrices con dimensiones "
+                                       "diferentes a M1(m,n) x M2(n,t)")
+
+        result = [[[] for j in range(len(m2[0]))] for i in range(len(m1))]
+        for i in range(len(m1)):
+            for j in range(len(m2[0])):
+                result[i][j] = 0
+                for k in range(len(m2)):
+                    result[i][j] += round(m1[i][k] * m2[k][j], 4)
+        return result
+
+    def cifrar(self, password):
+        result = ""
+        for char in password:
+            result += chr(ord(char)-30)
+        return result
+
+    def descifrar(self, password):
+        result = ""
+        for char in password:
+            result += chr(ord(char)+30)
+        return result
 
 if __name__ == "__main__":
     handler = CalculadoraHandler()
@@ -38,6 +166,6 @@ if __name__ == "__main__":
 
     server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
 
-    print("iniciando servidor...")
+    print("Iniciando servidor...")
     server.serve()
-    print("fin")
+    print("Fin")
